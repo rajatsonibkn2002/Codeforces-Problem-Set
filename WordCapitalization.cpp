c8e61575e8ced7148a1d7d7d82b698bd55116335
Rajat Soni <rajatsonibkn2002@gmail.com>
2020-10-27 12:17:28 +0530

    Solution in C++ Language

---
+++ WordCapitalization.cpp
@@ -0,0 +1,13 @@
+//Problem Link: http://codeforces.com/problemset/problem/281/A
+/*
+  Author: Rajat Soni
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string a;
+    cin>>a;
+    a[0] = toupper(a[0]);
+    cout<<a;
+}
