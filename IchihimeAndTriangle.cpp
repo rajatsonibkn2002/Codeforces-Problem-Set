e4fa804dc23b166eac070fa0569cee0b4d3d7b86
Rajat Soni <rajatsonibkn2002@gmail.com>
2020-11-16 22:40:01 +0530

    Solution in C++ Language

---
+++ IchihimeAndTriangle.cpp
@@ -0,0 +1,34 @@
+//Problem Link: https://codeforces.com/problemset/problem/1337/A
+/*
+  Author: Rajat Soni
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+// typedef long l;
+// typedef long long ll;
+typedef vector<int> vi;
+// typedef vector<char> vc;
+typedef std::vector<int>::iterator Auto;
+// #define PB push_back
+#define FOR(i,a,b)  for(int i=a;i<b;i++)
+#include <vector>
+
+
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input","r",stdin); 
+    freopen("output","w",stdout);
+    #endif
+
+    int t, a, b, c, d;
+    cin>>t;
+    FOR(i, 0, t){
+    	cin>>a>>b>>c>>d;
+    	cout<<b<<" "<<c<<" "<<c<<endl;
+    }
+}
+
+
