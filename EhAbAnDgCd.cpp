14f15cc595632ca04ec56f5fd448d01007ca266a
Rajat Soni <rajatsonibkn2002@gmail.com>
2020-11-18 23:42:57 +0530

    Solution in C++ Language

---
+++ EhAbAnDgCd.cpp
@@ -0,0 +1,37 @@
+//Problem Link: https://codeforces.com/problemset/problem/1325/A
+/*
+  Author: Rajat Soni
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+// typedef long l;
+// typedef long long ll;
+typedef vector<int> vi;
+// typedef vector<char> vc;
+typedef std::vector<int>::iterator Auto;
+// #define PB push_back
+#define FOR(i,a,b)  for(int i=a;i<b;i++)
+#include <vector>
+
+
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input","r",stdin); 
+    freopen("output","w",stdout);
+    #endif
+
+    long long int t, n;
+    cin>>t;
+    FOR(i, 0, t){
+    	cin>>n;
+    	cout<<1<<" "<<n-1<<endl;
+    }
+}
+
+
+
+
+
