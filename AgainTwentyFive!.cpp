b3f0e4591cfe0be7f90f920b744482c8548c8dcb
Rajat Soni <rajatsonibkn2002@gmail.com>
2020-11-19 21:25:35 +0530

    Solution in C++ Language

---
+++ AgainTwentyFive!.cpp
@@ -0,0 +1,34 @@
+//Problem Link: https://codeforces.com/problemset/problem/630/A
+/*
+  Author: Rajat Soni
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+// typedef long l;
+// typedef long long ll;
+typedef vector<int> vi;
+// typedef vector<char> vc;
+typedef std::vector<int>::iterator Auto;
+// #define PB push_back
+#define FOR(i,a,b)  for(int i=a;i<b;i++)
+#include <vector>
+
+
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input","r",stdin); 
+    freopen("output","w",stdout);
+    #endif
+
+    int n;
+    cin>>n;
+    cout<<25;
+}
+
+
+
+
+
