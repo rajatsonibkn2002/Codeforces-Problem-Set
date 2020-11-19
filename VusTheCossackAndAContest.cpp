63b126104fe750c1e736f4e05d7e7201ab58263a
Rajat Soni <rajatsonibkn2002@gmail.com>
2020-11-19 20:49:54 +0530

    Solution in C++ Language

---
+++ VusTheCossackAndAContest.cpp
@@ -0,0 +1,37 @@
+//Problem Link: https://codeforces.com/problemset/problem/1186/A
+/*
+  Author: Rajat Soni
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+// typedef long l;
+// typedef long long ll;
+typedef vector<int> vi;
+// typedef vector<char> vc;
+typedef std::vector<int>::iterator Auto;
+// #define PB push_back
+#define FOR(i,a,b)  for(int i=a;i<b;i++)
+#include <vector>
+
+
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input","r",stdin); 
+    freopen("output","w",stdout);
+    #endif
+
+    int n, m, k;
+    cin>>n>>m>>k;
+    if(min(m, k)>=n)
+    	cout<<"YES";
+    else
+    	cout<<"NO";
+}
+
+
+
+
+
