005158a9a95702c4eefead6eaf77cdc479ccca16
Rajat Soni <rajatsonibkn2002@gmail.com>
2020-12-06 14:32:08 +0530

    Solution in C++ Language

---
+++ Panoramix'sPrediction.cpp
@@ -0,0 +1,47 @@
+//Problem Link: https://codeforces.com/problemset/problem/80/A
+/*
+  Author: Rajat Soni
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+// typedef long l;
+// typedef long long ll;
+typedef vector<int> vi;
+// typedef vector<char> vc;
+typedef std::vector<int>::iterator Auto;
+// #define PB push_back
+#define FOR(i,a,b)  for(int i=a;i<b;i++)
+#include <vector>
+
+
+
+bool isPrime(int num){
+    bool flag=true;
+    for(int i = 2; i <= num / 2; i++) {
+       if(num % i == 0) {
+          flag = false;
+          break;
+       }
+    }
+    return flag;
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input","r",stdin); 		
+    freopen("output","w",stdout);
+    #endif
+
+    int n, m, out;
+    cin>>n>>m;
+    out=n+1;
+    while(!isPrime(out)){
+    	out++;
+    }	
+    if(out==m)
+    	cout<<"YES";
+    else
+    	cout<<"NO";
+}
