4c28ba37bb278ab4402306abfb6f87a239a5e14c
Rajat Soni <rajatsonibkn2002@gmail.com>
2021-01-19 13:30:26 +0530

    Solution in C++ Language

---
+++ SplittingIntoDigits.cpp
@@ -0,0 +1,41 @@
+//Problem Link: https://codeforces.com/problemset/problem/1104/A
+/*
+  Author: Rajat Soni
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+// typedef long l;
+// typedef long long ll;
+typedef vector<int> vi;
+// typedef vector<char> vc;
+typedef std::vector<int>::iterator Auto;
+// #define PB push_back
+#define FOR(i,a,b)  for(long long int i=a;i<b;i++)
+#include <vector>
+
+
+long long gcd(long long int a, long long int b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+long long lcm(int a, int b)
+{
+    return (a / gcd(a, b)) * b;
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input","r",stdin);         
+    freopen("output","w",stdout);
+    #endif
+        
+    int k;
+    cin>>k;
+    cout<<k<<endl;
+    FOR(i, 0, k){
+        cout<<1<<" ";
+    }
+}
