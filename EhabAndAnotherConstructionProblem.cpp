7a510637b768a3322feb46625581801091299f8e
Rajat Soni <rajatsonibkn2002@gmail.com>
2021-01-24 12:28:51 +0530

    Solution in C++ Language

---
+++ EhabAndAnotherConstructionProblem.cpp
@@ -0,0 +1,31 @@
+//Problem Link: https://codeforces.com/problemset/problem/1088/A
+/*
+  Author: Rajat Soni
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+// typedef long l;
+// typedef long long ll;
+typedef vector<int> vi;
+// typedef vector<char> vc;
+typedef std::vector<int>::iterator Auto;
+// #define PB push_back
+#define FOR(i,a,b)  for(long long int i=a;i<b;i++)
+#include <vector>
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input","r",stdin);         
+    freopen("output","w",stdout);
+    #endif
+            
+    int n;
+    cin>>n;
+    if(n==1)
+        cout<<-1;
+    else
+        cout<<n<<' '<<n;
+
+}
