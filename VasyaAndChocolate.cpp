e1a0068af530ffe7d6e7733045dea128588950b1
Rajat Soni <rajatsonibkn2002@gmail.com>
2021-01-27 21:53:21 +0530

    Solution in C++ Language

---
+++ VasyaAndChocolate.cpp
@@ -0,0 +1,43 @@
+//Problem Link: https://codeforces.com/problemset/problem/1065/A
+/*
+  Author: Rajat Soni
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+// typedef long l;
+// typedef long long ll;
+typedef vector<int> vi;
+// typedef vector<char> vc;
+typedef std::vector<int>::iterator Auto;
+// #define PB push_back
+#define FOR(i,a,b)  for(long long int i=a;i<b;i++)
+#include <vector>
+
+
+bool isPrime(int num){
+    if(num == 1) return false;
+    for(int i = 2;i<=sqrt(num);i++){
+        if(num % i == 0) return false;
+    }
+    return true;
+}
+
+
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input","r",stdin);         
+    freopen("output","w",stdout);
+    #endif
+    
+    long long int t, s, a, b, c;
+    cin>>t;
+    FOR(i, 0, t){
+        cin>>s>>a>>b>>c;
+        cout<<(s/c)+(((s/c)/a)*b)<<endl;    
+    }
+    
+
+}
