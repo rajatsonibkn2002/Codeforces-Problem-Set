83fb0d68b7307061d4125aed1d44115e856cd5e0
Rajat Soni <rajatsonibkn2002@gmail.com>
2021-01-31 15:49:00 +0530

    Solution in C++ Language

---
+++ Partition.cpp
@@ -0,0 +1,46 @@
+//Problem Link: https://codeforces.com/problemset/problem/946/A
+/*
+  Author: Rajat Soni
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+// typedef long l;
+// typedef long long ll;
+typedef vector<int> vi;
+// typedef vector<char> vc;
+typedef std::vector<int>::iterator Auto;
+// #define PB push_back
+#define FOR(i,a,b)  for(long long int i=a;i<b;i++)
+#include <vector>
+
+
+bool isPrime(int num){
+    if(num == 1) return false;
+    for(int i = 2;i<=sqrt(num);i++){
+        if(num % i == 0) return false;
+    }
+    return true;
+}
+
+
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input","r",stdin);         
+    freopen("output","w",stdout);
+    #endif
+    
+    int n, an, pos=0, neg=0;
+    cin>>n;
+    FOR(i, 0, n){
+        cin>>an;
+        if(an>0)
+            pos=pos+an;
+        else
+            neg=neg+an;
+    }
+    cout<<pos-neg;
+
+}
