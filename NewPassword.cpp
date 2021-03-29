d0b5e0b7039459940344d5953a48ad9ae9538040
Rajat Soni <rajatsonibkn2002@gmail.com>
2021-03-29 13:09:36 +0530

    Solution in C++ Language

---
+++ NewPassword.cpp
@@ -0,0 +1,38 @@
+//Problem Link: https://codeforces.com/problemset/problem/770/A
+/*
+  Author: Rajat Soni
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<char> vc;
+typedef vector<string> vs;
+
+#define PB push_back
+#define MP make_pair
+
+#define FOR(i,a,b)  for(int i=a;i<b;i++)
+
+
+
+int main()
+{
+    #ifndef ONLINE_JUDGE    
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+
+    int n, k;
+    cin>>n>>k;
+    while(n>0){
+        cout<<char(97+(n%k));
+        n--;
+    }
+
+
+}
